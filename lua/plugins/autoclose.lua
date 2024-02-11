e9a8b74b7f95eb686f6fab90b18ec6e240459f83
Wade Jones <wadedjones@gmail.com>
2024-02-11 15:04:59 -0800

    organized plugins for neovim setup

--- lua/plugins/autoclose.lua
+++ lua/plugins/autoclose.lua
@@ -1,5 +1,3 @@
-local autoclose = {}
-
 local config = {
 	keys = {
 		["("] = { escape = false, close = true, pair = "()" },
---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,12 @@
+return {
+    'nvim-lualine/lualine.nvim',
+    -- See `:help lualine.txt`
+    opts = {
+      options = {
+        icons_enabled = false,
+        theme = 'gruvbox-material',
+        component_separators = '',
+        section_separators = '',
+      },
+    },
+}
